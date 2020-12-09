b8d6c970327d60d01c2a51dc1e82e14c18074858
Bit <willian.souza.ti@gmail.com>
2020-12-08 22:34:02 -0300

    fix doll

--- JavaProject/src/univali/bit/View/Doll.java
+++ JavaProject/src/univali/bit/View/Doll.java
@@ -2,7 +2,7 @@ package univali.bit.View;
 
 import univali.bit.Command;
 
-public class Doll extends Command {
+public class Doll {
     private int wrongErrors = 0;
 
     public void setWrongErrors(int wrongErrors) {
@@ -28,29 +28,5 @@ public class Doll extends Command {
                 (leftLeg ? "/" : " ")+
                 (rightLeg ? " \\" : " ")+
                 "\n------------\n";
-    }
-
-    @Override
-    public void Execute(){
-        boolean head = this.wrongErrors >= 1;
-        boolean body = this.wrongErrors >= 2;
-        boolean leftArm = this.wrongErrors >= 3;
-        boolean rightArm = this.wrongErrors >= 4;
-        boolean leftLeg = this.wrongErrors >= 5;
-        boolean rightLeg = this.wrongErrors >= 6;
-
-        String doll = "+=====+" +
-                "\n||    |"+
-                "\n||    " + (head ? "o" : " ")+
-                "\n||   " +
-                (leftArm ? "/" : " ")+
-                (body ? "|" : " ")+
-                (rightArm ? "\\" : " ")+
-                "\n||   "+
-                (leftLeg ? "/" : " ")+
-                (rightLeg ? " \\" : " ")+
-                "\n------------\n";
-
-        System.out.println(doll);
     }
 }
