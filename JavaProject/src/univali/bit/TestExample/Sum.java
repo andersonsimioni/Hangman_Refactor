bbc7c7ea7661c5cb57b6fcc9ff65eee70962e037
Bit <willian.souza.ti@gmail.com>
2020-12-08 21:11:59 -0300

    Simple skeleton

---
+++ JavaProject/src/univali/bit/TestExample/Sum.java
@@ -0,0 +1,17 @@
+package univali.bit.TestExample;
+
+public class Sum {
+    private int x = 0;
+    private int y = 0;
+
+    public Sum(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getSum()
+    {
+        return this.x + this.y;
+    }
+}
---
+++ JavaProject/tests/TestSum.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import univali.bit.TestExample.Sum;
+
+public class TestSum {
+
+    @Test
+    public void sumTwoNumbers() {
+        Sum tester = new Sum(10, 10); // MyClass is tested
+
+        // assert statements
+        assertEquals(20, tester.getSum(), "10 + 10 must be 20");
+    }
+}
